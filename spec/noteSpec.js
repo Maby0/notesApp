d254e280df23a2ddfb3edb0f115faaaec1042445
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-10-05 16:47:42 +0100

    completed note and unit tests

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,11 +1,16 @@
 'use strict';
 
 let note = new Note("this is the text on the note.");
+let longNote = new Note("this is a very long long long long long note")
 
 it('is an instance of the Note class', () => {
   expect(note).toBe(Note);
 })
 
-it('stores the text as a property within the note instance', () => {
-  expect(note.text).toEqual("this is the text on the note.");
+it('access the text as a property within the note instance', () => {
+  expect(note.getAllText()).toEqual("this is the text on the note.");
+})
+
+it('abbreviates the note to 20 characters if longer than 20', () => {
+  expect(longNote.abbreviate()).toEqual('this is a very long ');
 })
--- src/note.js
+++ src/note.js
@@ -4,4 +4,16 @@ class Note {
   constructor(noteText) {
     this.text = noteText;
   }
+
+  getAllText() {
+    return this.text;
+  }
+
+  abbreviate() {
+    if (this.text.length > 20) {
+      return this.text.substring(0, 20);
+    } else {
+      return this.text;
+    }
+  }
 }
